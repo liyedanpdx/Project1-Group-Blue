6b56d16d9039f585185fb016ebfdfa0aa04ec7b1
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-06-27 22:46:08 -0500

    combine

--- js/script.js
+++ js/script.js
@@ -1,37 +1,22 @@
 
-// Footer: Dropdown button
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdownLinks = document.querySelectorAll('.nav-link.dropdown-toggle');
-    
-    dropdownLinks.forEach(function(dropdownLink) {
-        dropdownLink.addEventListener('show.bs.dropdown', function() {
-            dropdownLink.classList.add('active-dropdown');
-        });
-        
-        dropdownLink.addEventListener('hide.bs.dropdown', function() {
-            dropdownLink.classList.remove('active-dropdown');
-        });
-    });
-});
+
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
 
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
 
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
